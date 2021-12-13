ec88d1ab415de56d10edb31727e4bc855c00bee3
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-12-13 18:36:18 +0100

    fix typo in config/bundle.php

--- config/bundles.php
+++ config/bundles.php
@@ -15,3 +15,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+}
